3c9f9839af610afd060993d40752c49e5c8804e7
laurensversluis <laurens_versluis@hotmail.com>
2016-09-20 18:33:07 +0100

    Workflow

---
+++ main.py
@@ -0,0 +1,20 @@
+# Choose layer
+
+# Copy Style
+
+# Choose composer
+
+# Choose columns
+
+# 1) GENERATE
+
+# Create layer group
+
+# Duplicate layer using style
+
+# 2) EXPORT
+
+# Switch on / off layers > different visibilities
+
+# Export image
+
